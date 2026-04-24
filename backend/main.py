0ef54d65177560e776f3dd505aeaf09c97afbdaa
Joshua <sorochijoshua30@gmail.com>
2026-04-24 16:32:06 +0100

    udpate

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,8 @@ ALLOWED_ORIGINS = os.getenv(
     "ALLOWED_ORIGINS",
     "http://localhost:5500,"
     "http://127.0.0.1:5500,"
+    "http://localhost:3000,"
+    "http://127.0.0.1:3000,"
     "http://localhost:8000,"
     "https://acpeprsy.vercel.app"
 ).split(",")
